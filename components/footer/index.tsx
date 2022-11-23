61b7d0e145deb21d7b4b7dbd667ea0524f2bd0a5
Anton Prokopenko <aprokopenko.dev@gmail.com>
2022-11-23 16:53:03 -0700

    add company logo on desktop version  & edit footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="bg-stone-400">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div className="relative flex h-16 items-center justify-center">
+                <div className="relative flex h-10 items-center justify-center">
                     <span className="text-center xs:text-xs sm:text-sm lg:text-sm text-white">
                         © {new Date().getFullYear()} Copyright
                     </span>
