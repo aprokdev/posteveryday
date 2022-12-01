829d438ceb7c37d408cf2431bf37679e4f03d204
Anton Prokopenko <aprokopenko.dev@gmail.com>
2022-11-30 20:27:39 -0700

    test AWS CI/CD

--- pages/add-post/index.tsx
+++ pages/add-post/index.tsx
@@ -6,7 +6,7 @@ function AddPost() {
     const [value, setValue] = React.useState('');
     return (
         <Layout>
-            <h2 className="text-3xl">In development</h2>
+            <h2 className="text-3xl">In development ¯\_(ツ)_/¯</h2>
             {/* <Input value={value} onChange={({ target }) => setValue(target.value)} /> */}
         </Layout>
     );
