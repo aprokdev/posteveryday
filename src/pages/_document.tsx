04c5365782a6a54a5cec663cda3be1e9c97c03d8
Anton Prokopenko <aprokopenko.dev@gmail.com>
2023-03-15 20:17:08 -0600

    edited db link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,16 +3,6 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html>
-            {/* <Head>
-                <title>POSTEVERYDAY</title>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1, user-scalable=1"
-                />
-                <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32.png" />
-                <link rel="icon" type="image/png" sizes="96x96" href="icons/favicon-96.png" />
-            </Head> */}
             <Head />
             <body>
                 <Main />
