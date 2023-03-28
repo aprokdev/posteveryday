ea782d41c6eb8fbad51499bf41d2d9c240513b18
Anton Prokopenko <aprokopenko.dev@gmail.com>
2023-03-28 12:39:35 -0600

    edited aws bucket region

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,6 +2,7 @@ import { S3Client } from '@aws-sdk/client-s3';
 import { PrismaClient } from '@prisma/client';
 
 export const s3 = new S3Client({
+    region: 'ca-central-1',
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
